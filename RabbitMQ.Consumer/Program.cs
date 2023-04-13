f8425a4e1c4bc4ce0d4d6bf8053b0bab8003f6a2
mustafayilmaz <mustafayilmaz@limak.com.tr>
2023-04-13 10:51:15 +0300

    Add project files.

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+
+// Bağlantı Oluşturma
+
+ConnectionFactory factory = new ();
+factory.Uri = new Uri("");
+
+// Bağlantı aktifleştirme ve kanal açma
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+// Queue oluşturma
+
+channel.QueueDeclare(queue: "example-queue", exclusive: false); // Consumer'da da kuyruk publisherdaki ile bire bir aynı yapılandırmada tanımlanmalıdır.
+
+
+// Queue dan mesaj okuma.
+
+// Channel üzerinden bir event operasyonu için
+
+EventingBasicConsumer consumer = new (channel);
+
+channel.BasicConsume(queue: "example-queue", autoAck: true, consumer);
+
+while (true)
+{
+
+    consumer.Received += (sender, e) =>
+    {
+        // Kuyruktan gelen mesajın işlendiği yer.
+
+        // e.Body : Kuyruktaki mesajın verisini bütünsel olarak getirecektir.
+
+        // e.Body.Span veya e.Body.ToArray() : Kuyruktaki mesajın byte mesajını verecektir.
+
+        Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+
+    };
+    Console.ReadLine();
+}
+
+
